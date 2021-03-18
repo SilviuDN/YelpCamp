863478c5fe0caa8f500a436ec11475e71062612a
Silviu Dilimot <silviudilimot@gmail.com>
2021-03-18 12:20:51 +0100

    Edit route, Delete route

--- seeds/index.js
+++ seeds/index.js
@@ -21,7 +21,7 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for(let i = 0; i<50; i++){
         const random1000 = Math.floor( Math.random() * 1000);
-        const random18 = Math.floor( Math.random() * 18);
+        // const random18 = Math.floor( Math.random() * 18);
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             // title: `${descriptors[random18]} ${places[random18]}`,
