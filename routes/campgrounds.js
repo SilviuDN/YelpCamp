cc93c3dbd1a55d9f204dc58045a4610bec3479ce
Silviu Dilimot <silviudilimot@gmail.com>
2021-03-22 12:13:16 +0100

    Router.route - restructuring routes

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -8,21 +8,18 @@ const {isLoggedIn, validateCampground, isAuthor, validateReview} = require('../m
 const ExpressError = require('../utils/ExpressError');
 const Campground = require('../models/campground');
 
-
-
-router.get('/', catchAsync( campgrounds.index ));
-
-router.post('/', isLoggedIn, validateCampground, catchAsync( campgrounds.createCampground ));
+router.route('/')
+    .get(catchAsync( campgrounds.index ))
+    .post(isLoggedIn, validateCampground, catchAsync( campgrounds.createCampground ));
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
-router.get('/:id', catchAsync( campgrounds.showCampground));
+router.route('/:id')
+    .get( catchAsync( campgrounds.showCampground))
+    .put( isLoggedIn, isAuthor, validateCampground, catchAsync( campgrounds.updateCampground))
+    .delete( isAuthor, isLoggedIn, catchAsync( campgrounds.deleteCampground));
 
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync( campgrounds.renderEditForm ));
-
-router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync( campgrounds.updateCampground));
-
-router.delete('/:id', isAuthor, isLoggedIn, catchAsync( campgrounds.deleteCampground));
 
 
 
--- routes/users.js
+++ routes/users.js
@@ -6,19 +6,18 @@ const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
 const users = require('../controllers/users');
 
-router.get('/register', users.renderRegister);
 
-router.post('/register', catchAsync( users.register));
+router.route('/register')
+    .get( users.renderRegister)
+    .post( catchAsync( users.register));
 
-
-router.get('/login', users.renderLogin)
-
+router.route('/login')
+    .get( users.renderLogin)
+    .post( passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), users.login)
 // we can have a different route to auth google, fbk etc.
 // options:
     // - failure flash - flashes message automatically
     // - failure redirect - redirects if falure
-router.post('/login', passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), users.login)
-
 
 router.get('/logout', users.logout)
 
